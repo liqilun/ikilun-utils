2ac7e3ef6ccb4d70faf3137d722bfb5b6627cad4
李启仑 <lun.li@gewara.com>
2017-06-16 10:19:24 +0800

    s

--- src/main/java/com/ikilun/util/StringUtil.java
+++ src/main/java/com/ikilun/util/StringUtil.java
@@ -1,5 +1,9 @@
 package com.ikilun.util;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class StringUtil {
 	/**
 	 * 判断字符是否是英文字母.
@@ -11,4 +15,17 @@ public class StringUtil {
 	public static boolean isAlpha(char c) {
 		return c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z';
 	}
+	public static String formatNumber(Number number) {
+		if (number == null) {
+			return "0";
+		}
+		NumberFormat formatter = new DecimalFormat("###,##0.##");  
+		return formatter.format(number);  
+	}
+	/**
+	 * 分转元
+	 */
+	public static BigDecimal changeF2Y(long amount) throws Exception{    
+        return BigDecimal.valueOf(Long.valueOf(amount)).divide(new BigDecimal(100));    
+    }
 }
